b36cd8c0dff4a7d7c7161abd0250af5c370377b7
Marco <51382551+maurelio631@users.noreply.github.com>
2024-05-22 19:35:06 +0000

    Adicionando o speedinsights da vercel ao portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ChakraProvider } from '@chakra-ui/react'
 import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
@@ -8,7 +9,8 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App/>
+      <App />
+      <SpeedInsights />
     </ChakraProvider>
   </React.StrictMode>
 )
